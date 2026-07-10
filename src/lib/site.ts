b598bcd36dcae99f1c89317fe3866d3a4fc504db
rajeesanjee <srstoresvadapalani@gmail.com>
2026-07-10 14:09:58 +0000

    v0.2 Initial homepage

---
+++ src/lib/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "Rajalakshmi Stores",
+  tagline: "Wholesale & Retail Provisions",
+  description:
+    "Premium grocery shopping with quality products at wholesale prices.",
+  phone: "+91-7358653901",
+  alternatePhone: "+91-8925112312",
+  email: "support@rajalakshmistores.in",
+};
